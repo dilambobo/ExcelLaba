b69adfbd7d034e2cc4bb2a98aacc29db83eb64c4
dilambobo <dasha.days24@gmail.com>
2022-04-28 22:41:51 +0300

    creating user interface

---
+++ src/main/java/com/example/ecxellab/FirstPageController.java
@@ -0,0 +1,19 @@
+package com.example.ecxellab;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FirstPageController {
+    @FXML
+    private Button importButton;
+
+    @FXML
+    private Button exportButton;
+
+    @FXML
+    protected void importFile() {}
+    @FXML
+    protected void exportFile() {}
+
+}
